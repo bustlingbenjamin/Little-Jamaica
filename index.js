03da57924063c0e59497aa417b0a2a84869ab109
lpruiettjr <60082307+lpruiettjr@users.noreply.github.com>
2020-08-19 10:28:58 -0500

    initialize respository

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// HOME button needs to be clickable
+//pictures in a column on these
+//footer needs to be on all tabs
+//about us - make in HTML and call on js
+//missoin statement
+// hours of operation- make in html call in javascript
